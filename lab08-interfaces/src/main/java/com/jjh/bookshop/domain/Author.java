f4c64f78385621ebcfef5e996f5a8263176e0dca
John Hunt <john.hunt10@gmail.com>
2023-07-14 17:30:54 +0100

    Initial Import

---
+++ lab08-interfaces/src/main/java/com/jjh/bookshop/domain/Author.java
@@ -0,0 +1,44 @@
+package com.jjh.bookshop.domain;
+
+public class Author implements PrettyPrinter {
+	
+	private String name;
+	private Address address;
+
+	public Author(String name) {
+		this.name = name;
+	}
+	
+	public Author(String name, Address address) {
+		this.name = name;
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Author [name=%s, address=%s]", name, address);
+	}
+
+	// Implements the PrettyPrinter interface
+
+	public void prettyPrint() {
+		System.out.printf("Author [name=%s, address=%s]%n", name, address);
+	}
+
+}
---
+++ lab08-interfaces/src/main/java/com/jjh/bookshop/domain/Publisher.java
@@ -0,0 +1,39 @@
+package com.jjh.bookshop.domain;
+
+public class Publisher implements PrettyPrinter {
+	
+	private String name;
+	private Address address;
+	
+	public Publisher(String name, Address address) {
+		super();
+		this.name = name;
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Publisher [name=%s, address=%s]", name, address);
+	}
+
+	public void prettyPrint() {
+		System.out.printf("Publisher [name=%s, address=%s]", name, address);
+	}
+	
+}
