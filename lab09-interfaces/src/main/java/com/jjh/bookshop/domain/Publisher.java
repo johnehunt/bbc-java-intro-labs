d0a6237acd17daca16abe3f91b182351a06dab0e
John Hunt <john.hunt10@gmail.com>
2023-08-02 10:18:52 +0100

    Added missed example

--- lab09-interfaces/src/main/java/com/jjh/bookshop/domain/Publisher.java
+++ lab09-interfaces/src/main/java/com/jjh/bookshop/domain/Publisher.java
@@ -1,39 +1,30 @@
-package com.jjh.bookshop.domain;
+package uk.co.bbc.bookshop.data;
 
-public class Publisher implements PrettyPrinter {
-	
-	private String name;
-	private Address address;
-	
-	public Publisher(String name, Address address) {
-		super();
-		this.name = name;
-		this.address = address;
+public class Publisher extends Contact implements PrettyPrinter {
+
+	private String organisation;
+
+	public Publisher(String name, Address address, String organisation) {
+		super(name, address);
+		this.organisation = organisation;
 	}
 
-	public String getName() {
-		return name;
+
+	public String getOrganisation() {
+		return organisation;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Address getAddress() {
-		return address;
-	}
-
-	public void setAddress(Address address) {
-		this.address = address;
+	public void setOrganisation(String organisation) {
+		this.organisation = organisation;
 	}
 
 	@Override
 	public String toString() {
-		return String.format("Publisher [name=%s, address=%s]", name, address);
+		return String.format("Publisher %s, organisation=%s", organisation, super.toString());
 	}
 
 	public void prettyPrint() {
-		System.out.printf("Publisher [name=%s, address=%s]", name, address);
+		System.out.printf("Publisher [name=%s, address=%s]", getName(), getAddress());
 	}
-	
+
 }
